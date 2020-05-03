b30485c64d9b321d9ef7da2b0e5bef7bb8049e01
yutaiwasawa <yutaiwasawa1212@gmail.com>
2020-05-03 15:34:31 +0900

    images with image-webpack-loader

--- src/javascripts/my.js
+++ src/javascripts/my.js
@@ -1,3 +1,6 @@
 export default () => {
-  console.log('this is module')
+  const obj = {a: 1, b: 2}
+  const newObj = {...obj, c: 3}
+  // {a: 1, b: 2, C: 3}
+  console.log('this is module', newObj)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: './src/javascripts/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -12,13 +14,30 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {'targets':'> 0.25%, not dead'}]
+              ]
+            }
+          }
+        ]
+      },
+      {
         test: /\.(css|sass|scss)/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader
           },
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
+            options: {
+              sourceMap: false
+            }
           },
           {
             loader: 'sass-loader'
@@ -26,13 +45,22 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpg)/,
+        test: /\.(png|jpg|jpeg)/,
         use: [
           {
             loader: 'file-loader',
             options: {
               esModule: false,
               name: 'images/[name].[ext]'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              }
             }
           }
         ]
